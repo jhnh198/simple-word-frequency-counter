c7943777468f1d2b472deffff69ce9081181a713
John Hewitt <jhnh198@gmail.com>
2025-02-07 21:04:33 -0600

    changed grammar guide

--- utils/text_content/grammar_guide.js
+++ utils/text_content/grammar_guide.js
@@ -1,62 +1,127 @@
 export const grammar_guide = {
   text: `
-  # Grammar Guide
+  ## **い-adjectives**
 
-  verbs 
-  -る verbs - ends in ru
-  -う verbs - ends with any other hiragana character
-  -irregular verbs - special verbs 
-    -する
-    -くる
-    -いる
+| **Dictionary** | **Past (た)** | **Negative (ない)** | **Potential (ば)** |
+| --- | --- | --- | --- |
+| 美味しい | 美味しかっ**た** | 美味しく**ない** | 美味しけれ**ば** |
+| Delicious | Was delicious | Not delicious | If it’s delicious |
+| 寒い | 寒かっ**た** | 寒く**ない** | 寒けれ**ば** |
+| Cold | Was cold | Not cold | If it’s cold |
 
-  - dictionary form: basic form/ neutral or informal form
-  -ます form: polite form
-  -て form: connects/groups actions together
-  -た form: past form
-  -ない form:negative form
-  -たい form: desire, want to do something
-  -ば: conditional form
-  - たがる: showing someone else's desire
-  -volitional form
-  -potential form
-  -passive form
-  -causative form
-  -causative-passive form
-  -imperative form
-  -provisional conditional form
+## **な-adjectives**
 
-  particles
-  -は: subject(general) particle
-  -が: subject(emphasis or questions) particle
-  -を: direct object particle
-  -に: location(to,at) particle
-  -で: location particle
-  -と: and
-  -や: and(set of things)
-  -へ: direction particle
-  -から: from particle
-  -まで: until particle
-  -の: possessive particle
-  -よ: emphasis particle
-  -ね: confirmation particle
-  -か: question particle
-  -も: also particle
-  -だけ: only particle
-  -しか: only and nothing more particle
-  -ばかり: only particle
-  -さえ: even, sometimes, only particle; conditional particle
-  -とき: when particle
-  -ため: for particle
-  -わけ: reason particle
-  -ほど: extent particle
-  -より: comparison particle
-  -こと: nominalizer particle
-  -ながら: while particle
-  -にも: even particle
+Function like nouns, and modify other nouns with な between the two nouns.
 
-  adjectives
-  -i adjectives - adjective ends in i
-  -na adjectives - use na after the adjective and before the noun
+> 綺麗な場所だ = It is a pretty place (Pretty place is)
+> 
+
+な adjectives can also function as nouns
+
+> 場所は綺麗だ = The place is pretty. (Place pretty is)
+> 
+
+な adjectives conjugate like nouns paired with the copula, so see **な adjective** conjugations for reference.
+
+## Conjugating the Copula with **Nouns & な adjectives**
+
+Nouns and な adjectives conjugate with the copula だ as such.
+
+> だ (is) ー だっ**た** (was) ー じゃ**ない** (is not) ー
+> 
+
+> 本だ (is book) ー 本だっ**た** (was a book) ー 本じゃ**ない** (is not a book)
+> 
+
+> 有名だ (is famous) ー 有名だっ**た** (was famous) ー 有名じゃ**ない** (is not famous)
+> 
+
+## **Regular Verb conjugations**
+
+### **Dictionary form with stem colored**
+
+| 買う | 読む | 行く | 教える |
+| --- | --- | --- | --- |
+| To buy | To read | To go | To teach |
+
+### **Past tense conjugations**
+
+| 買う = 買っ**た** | 読む = 読ん**だ** | 行く= 行っ**た** | 教える=教え**た** |
+| --- | --- | --- | --- |
+| bought | read (past) | Went (“go” past) | Taught |
+
+### **Negative form conjugations**
+
+| 買う = 買わ**ない** | 読む = 読ま**ない** | 行く= 行か**ない** | 教える=教え**ない** |
+| --- | --- | --- | --- |
+| Will not buy | will not read | Will not go | Will not teach |
+
+### **Potential form conjugations**
+
+| 買う = 買**える** | 読む = 読**める** | 行く= 行**ける** | 教える=教え**られる** |
+| --- | --- | --- | --- |
+| Can buy | Can read | Can go | Can teach |
+
+### **Volitional form conjugations**
+
+| 買う = 買**おう** | 読む = 読**もう** | 行く= 行**こう** | 教える=教え**よう** |
+| --- | --- | --- | --- |
+| Let’s buy | Let’s read | Let’s go | Let’s teach |
+| I will buy | I will read | I will go | I will teach |
+| Shall we buy? | Shall we read? | Shall we go? | Shall we teach? |
+
+### **ば Hypothetical conditional form**
+
+| 買う = 買**えば** | 読む = 読**めば** | 行く= 行**けば** | 教える=教え**れば** |
+| --- | --- | --- | --- |
+| If I bought | If I read | If I went | If I taught |
+
+### **て (てる/ている） form - Present tense “doing form”.**
+
+| 買う = 買っ**てる**
+買う = 買っ**ている** | 読む = 読ん**でる**
+読む = 読ん**でいる** | 行く= 行っ**てる**
+行く= 行っ**ている** | 教える=教え**てる**
+教える=教え**ている** |
+| --- | --- | --- | --- |
+| Buying | Reading | Going | Teaching |
+
+## **Irregular Verbs - する 来る**
+
+| **Dictionary** | **Past** | **Negative** | **Volitional** | **Potential** | **ている** |
+| --- | --- | --- | --- | --- | --- |
+| する | した | しない | しよう | できる | してる |
+| To do | did | Won't do | Will do | Can do | doing |
+| 来る | きた | こない | こよう | こられる | きてる |
+| come | went | Won't come | Will come | Can come | coming |
+
+## **Particles:**
+
+が - The subject marking particle (emphasis on what comes before)
+
+は - The topic marking particle (emphasis on what comes after. pronounced わ as a particle)
+
+を - Object marking particle
+
+で - Where does an action happen?
+
+に - When does it happen?
+
+も - Also or Too
+
+と - Listing nouns in a complete list
+
+や - Listing nouns in an incomplete list
+
+か - Mark the unknown (make a statement a question)
+
+の - The possession marking particle
+
+## **Existence in two verbs - いる and ある**
+
+いる = Existence of living things
+
+ある = Existence of non living things.
   `
 }
+
