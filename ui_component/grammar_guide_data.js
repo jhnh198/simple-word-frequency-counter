8c350610e96df766285711fade983f1cda487c5b
John Hewitt <jhnh198@gmail.com>
2025-02-17 16:14:04 -0600

    Edited verb conjugation table

--- ui_component/grammar_guide_data.js
+++ ui_component/grammar_guide_data.js
@@ -9,9 +9,7 @@ export const grammar_guide_data = {
             headers: ["Dictionary", "Past (た)", "Negative (ない)", "Potential (ば)"],
             rows: [
               ["美味しい", "美味しかった", "美味しくない", "美味しければ"],
-              ["Delicious", "Was delicious", "Not delicious", "If it’s delicious"],
               ["寒い", "寒かった", "寒くない", "寒ければ"],
-              ["Cold", "Was cold", "Not cold", "If it’s cold"],
             ]
           }
         }
@@ -37,7 +35,7 @@ export const grammar_guide_data = {
       content: [
         {
           list: [
-            "だ (is) ー だった (was) ー じゃない (is not) ー",
+            "だ (is) ー だった (was) ー じゃない (is not)",
             "猫だ (is a cat) ー 猫だった (was a cat) ー 猫じゃない (is not a cat)",
             "綺麗だ (is pretty) ー 綺麗だった (was pretty) ー 綺麗じゃない (is not pretty)",
 
@@ -100,22 +98,8 @@ export const grammar_guide_data = {
       ],
     },
     {
-      header:"Existence in two verbs",
-      subheader: "",
-      content:[
-        {
-          table: {
-            headers: ["いる", "ある"],
-            rows: [
-              ["To exist (animate)", "To exist (inanimate)"],
-              ["いる is used for living things", "ある is used for inanimate objects"],
-            ]
-          }
-        }
-      ],
-    },
-    {
-      header:"Regular Verb conjugations",
+      //todo: check for accuracy
+      header:"Verb conjugations",
       subheader: "",
       content: [
         {
@@ -123,34 +107,12 @@ export const grammar_guide_data = {
             headers: ["Dictionary", "Past", "Negative", "Volitional", "Potential", "ている"],
             rows: [
               ["する","した", "しない", "しよう", "できる", "してる"],
-              ["来る", "きた", "こない", "こよう", "こられる", "きてる"]
+              ["来る", "きた", "こない", "こよう", "こられる", "きてる"],
+              ["買う", "買った", "買わない", "買おう", "買える", "買ってる"],
+              ["教える", "教えた", "教えない", "教えよう", "教えられる", "教えてる"],
             ]
           }
         },
-        {
-          table: {
-            headers: ["買う = 買った", "読む = 読んだ", "行く= 行った", "教える=教えた"],
-            rows: [
-              ["bought", "read (past)", "Went (“go” past)", "Taught"],
-            ]
-          }
-        },
-        {
-          table: {
-            headers: ["買う = 買わない", "読む = 読まない", "行く= 行かない", "教える=教えない"],
-            rows: [
-              ["Will not buy", "will not read", "Will not go", "Will not teach"],
-            ]
-          }
-        },
-        {
-          table: {
-            headers: ["買う = 買える", "読む = 読める", "行く= 行ける", "教える=教えられる"],
-            rows: [
-              ["Can buy", "Can read", "Can go", "Can teach"],
-            ]
-          }
-        }
       ]
     },
     {
