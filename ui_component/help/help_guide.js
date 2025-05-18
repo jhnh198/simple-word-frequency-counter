901fbc1ec83828567ab4588e2984d908e4b2cedf
John Hewitt <jhnh198@gmail.com>
2025-05-17 23:46:56 -0500

    Moved script copy to deprecated. Added help guide text

---
+++ ui_component/help/help_guide.js
@@ -0,0 +1,18 @@
+export default help_guide_data = {
+  all_data: [
+    {
+      header: "Help Guide",
+      subheader: "This is a help guide for the app.",
+      content: [
+        {
+          list: [
+            "",
+            "You can use the app to practice reading, writing, and speaking Japanese.",
+            "The app includes various features such as flashcards, quizzes, and grammar guides.",
+            "If you have any questions or feedback, please contact us at",
+          ]
+        }
+      ]
+    },
+  ]
+};
